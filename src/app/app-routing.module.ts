39571f9d7a8af6d63284b09344788eb7625eafe5
Varun Patil <radialapps@gmail.com>
2021-01-29 22:32:12 +0530

    Boostrap chat component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChatComponent } from './chat/chat.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'chat', component: ChatComponent },
+  { path: '**', redirectTo: '/chat', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
