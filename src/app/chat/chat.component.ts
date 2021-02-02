dd0acb84ead9b6d6f62b0c1915258506ccb22b42
Varun Patil <radialapps@gmail.com>
2021-02-03 04:25:25 +0530

    Implement CSS shenanigans

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ChangeDetectorRef, ElementRef, ViewChild } from '@angular/core';
 import { FwFace } from '@ndn/fw';
 import { Name } from "@ndn/packet";
 import { enableNfdPrefixReg } from "@ndn/nfdmgmt";
@@ -14,7 +14,22 @@ export class ChatComponent implements OnInit, OnDestroy {
   face: FwFace | null = null;
   sock: Socket | null = null;
 
-  constructor() {}
+  syncPrefix = '/ndn/svs';
+  public nodeId = 'dog';
+
+  typedMessage = '';
+
+  @ViewChild('scrollframe', {static: false}) scrollFrame: ElementRef;
+
+  public messages: {
+    nid: string,
+    msg: string,
+    seq: number,
+  }[] = [];
+
+  constructor(
+    private cdr: ChangeDetectorRef
+  ) {}
 
   async ngOnInit() {
     // Connect to websocket transport
@@ -26,24 +41,49 @@ export class ChatComponent implements OnInit, OnDestroy {
     enableNfdPrefixReg(face);
 
     // Sync prefix
-    const prefix = new Name('/ndn/svs');
+    const prefix = new Name(this.syncPrefix);
 
-    this.sock = new Socket(prefix, 'dog', face, (missingData) => {
+    // Start SVS socket
+    this.sock = new Socket(prefix, this.nodeId, face, (missingData) => {
       // For each node
       for (const m of missingData) {
         // Fetch at most last five messages
         for (let i = Math.max(m.high - 5, m.low); i <= m.high; i++) {
           this.sock?.fetchData(m.session, i).then((data) => {
             const msg = new TextDecoder().decode(data.content);
-            this.newMessage(m.session, msg);
-          }).catch();
+            this.newMessage(m.session, msg, i);
+          }).catch(() => {});
         }
       }
     });
   }
 
-  newMessage(sender: string, message: string) {
-    console.log(`${sender} => ${message}`);
+  private isUserNearBottom(scrollContainer: any): boolean {
+    const threshold = 150;
+    const position = scrollContainer.scrollTop + scrollContainer.offsetHeight;
+    const height = scrollContainer.scrollHeight;
+    return position > height - threshold;
+  }
+
+  newMessage(nid: string, msg: string, seq: number) {
+    this.messages.push({ nid, msg, seq });
+    this.cdr.detectChanges();
+
+    const scrollContainer = this.scrollFrame.nativeElement;
+    if (this.isUserNearBottom(scrollContainer)) {
+      scrollContainer.scroll({
+        top: this.scrollFrame.nativeElement.scrollHeight,
+        left: 0,
+        behavior: 'smooth',
+      });
+    }
+  }
+
+  sendMessage() {
+    if (!this.typedMessage) return;
+    this.sock?.publishData(new TextEncoder().encode(this.typedMessage), 4000);
+    this.newMessage(this.nodeId, this.typedMessage, (<Socket>this.sock).getLogic().getSeqNo());
+    this.typedMessage = '';
   }
 
   ngOnDestroy(): void {
