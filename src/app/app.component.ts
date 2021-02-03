65565b05452188d762c6e6e0c4deaf217a925f1a
Varun Patil <radialapps@gmail.com>
2021-02-03 17:57:21 +0530

    Flyout for no reason

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'svchat';
+  menuOpen = false;
+
+  toggleMenu(open: boolean) {
+    this.menuOpen = open;
+  }
 }
